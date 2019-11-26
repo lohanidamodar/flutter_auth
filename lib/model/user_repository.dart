3a200745b53f3e3dac9792ccdda3c294957e2e83
Damodar Lohani <dlohani48@gmail.com>
2019-11-27 05:12:44 +0545

    initial setup

---
+++ lib/model/user_repository.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
+
+class UserRepository with ChangeNotifier {
+  final FirebaseAuth auth;
+  FirebaseUser _user;
+  Status _status = Status.Uninitialized;
+
+  UserRepository.instance({this.auth}) {
+    auth.onAuthStateChanged.listen(onAuthStateChanged);
+  }
+
+  Status get status => _status;
+  FirebaseUser get user => _user;
+
+  Future<bool> signIn(String email, String password) async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future signOut() async {
+    auth.signOut();
+    _status = Status.Unauthenticated;
+    notifyListeners();
+    return Future.delayed(Duration.zero);
+  }
+
+  Future<void> onAuthStateChanged(FirebaseUser firebaseUser) async {
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = firebaseUser;
+      _status = Status.Authenticated;
+    }
+    notifyListeners();
+  }
+}
